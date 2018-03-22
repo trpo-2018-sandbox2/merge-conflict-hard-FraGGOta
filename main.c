324ad356fe5d1fd096b59170f0c8107b3c148a11
Рамус Евгений Сергеевич <is741s19@mwp04.csc.local>
2018-03-22 08:51:44 +0700

    Fixed main.c

--- main.c
+++ main.c
@@ -29,9 +29,8 @@ void print_array(int *arr, int size)
 int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
-
     const int min = min_element(arr, N_ELEMENTS(arr));
-
+    
     print_array(arr, N_ELEMENTS(arr));
     printf("Min element: %d\n", min);
 
